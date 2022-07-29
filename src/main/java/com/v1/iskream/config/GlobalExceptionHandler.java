439baa5b70e4a481aec2c2292230255a265f0c96
ju-ei8ht <qkr8125@gmail.com>
2022-07-29 21:26:32 +0900

    GlobalExceptionHandler

---
+++ src/main/java/com/v1/iskream/config/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.v1.iskream.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handlerException(Exception e){
+        String msg = e.getMessage();
+        return new ResponseEntity<>(
+                msg, HttpStatus.BAD_REQUEST
+        );
+    }
+}
