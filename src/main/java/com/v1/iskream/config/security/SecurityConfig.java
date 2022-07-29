32f8b56a8afc59896d5a2733cf1db0c9684edd91
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-07-29 20:55:41 +0900

    Build : Security Config

--- src/main/java/com/v1/iskream/config/security/SecurityConfig.java
+++ src/main/java/com/v1/iskream/config/security/SecurityConfig.java
@@ -16,7 +16,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
