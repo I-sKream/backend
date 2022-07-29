18935798d4c6df83c72620f81abf2563b9269331
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-07-29 15:38:43 +0900

    Init

---
+++ src/main/java/com/v1/iskream/IskreamApplication.java
@@ -0,0 +1,13 @@
+package com.v1.iskream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IskreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IskreamApplication.class, args);
+	}
+
+}
